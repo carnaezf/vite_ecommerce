4aeb9e208b77f4bbfb687df8f3db5838df748471
Cesar Arnaez Flores <c_arnaez.flores@hotmail.com>
2023-04-09 06:39:44 -0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Product from '../src/components/Product'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Titulo</h1>
+      <Product/>
+    </div>
+  )
+}
+
+export default App
