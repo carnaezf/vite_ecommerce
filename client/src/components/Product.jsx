6fb6560e5da3006b6e3556bdf76eb288c0317d83
Cesar Arnaez Flores <c_arnaez.flores@hotmail.com>
2023-04-12 11:55:06 -0400

    Se agrega icono de ShoppinCart

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -9,10 +9,11 @@ import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -31,6 +32,20 @@ export default function Product() {
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+
+  const addToBasket = () => {
+    dispatch({
+      type: actionTypes.ADD_TO_BASKET,
+      item: {
+        id,
+        name,
+        category,
+        sellingPrice,
+        description,
+        images
+      }
+    })
+  }
 
   return (
     <Card sx={{ maxWidth: 345 }}>
@@ -51,20 +66,16 @@ export default function Product() {
         component="img"
         height="194"
         image="https://assets.adidas.com/images/w_600,f_auto,q_auto/0ee4ef2f134d46beaefcac5c011c4bca_9366/EQT_Spikeless_Golf_Shoes_Grey_FX7449_06_standard.jpg"
-        alt="shoe"
+        alt="shoes"
       />
-      <CardContent> 
+      <CardContent>
         <Typography variant="body2" color="text.secondary">
-          "Put comfort first. These adidas spikeless golf shoes combine a Bounce midsole and a Boost 
-          heel for a lightweight, responsive feel that helps you convert bogie's to birdies. 
-          They repel water to keep your feet dry through wet fairways. The versatile rubber outsole 
-          ensures easy transitions from your final putt to cold drinks at the clubhouse. 
-          Recycled materials are one step towards reducing plastic waste."
+          shoes
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+        <IconButton  aria-label='Add to Cart' onClick={addToBasket} >
+          <AddShoppingCartIcon fontSize='large' />
         </IconButton>
         <IconButton aria-label="share">
           <ShareIcon />
@@ -80,29 +91,16 @@ export default function Product() {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Details:</Typography>
           <Typography paragraph>
             "EQT Spikeless Golf Shoes",
           </Typography>
           <Typography paragraph>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
-            in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum."
+            "Put comfort first. These adidas spikeless golf shoes combine a Bounce midsole and a Boost
+            heel for a lightweight, responsive feel that helps you convert bogie's to birdies.
+            They repel water to keep your feet dry through wet fairways. The versatile rubber outsole
+            ensures easy transitions from your final putt to cold drinks at the clubhouse.
+            Recycled materials are one step towards reducing plastic waste."
           </Typography>
-          <Typography paragraph>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
-            in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum."
-          </Typography>
-
         </CardContent>
       </Collapse>
     </Card>
